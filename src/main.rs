6d14030a60fb2a3c1db3b104971902fc9df6a0fa
Johan T. Larsson <johan.t.larsson@gmail.com>
2015-08-27 22:06:42 +0200

    Create window.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+#[macro_use]
+extern crate glium;
+
 fn main() {
+  use glium::DisplayBuild;
     
+  let window = glium::glutin::WindowBuilder::new()
+    .with_dimensions(1024, 768)
+    .with_title(format!("Fortress"))
+    .build_glium()
+    .unwrap();
+      
+  let mut running = true;
+  while running {
+    for event in window.poll_events() {
+      match event {
+        glium::glutin::Event::Closed => running = false,
+        _ => ()
+      }
+    }
+  }
 }
